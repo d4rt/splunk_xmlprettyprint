525448ee7bd3aab9a22b0525d050813630312c93
Duncan Turnbull <dunc@nturnbull.com>
2013-05-24 09:27:37 +0100

    initial commit

---
+++ appserver/modules/XMLResultsTable/XMLResultsTable.js
@@ -0,0 +1,7 @@
+//put Module in the namespace if it isnt already there.
+Splunk.namespace("Module");
+
+// A simple, but extensible, results table
+Splunk.Module.XMLResultsTable = $.klass(Splunk.Module.SimpleResultsTable, {
+
+});
